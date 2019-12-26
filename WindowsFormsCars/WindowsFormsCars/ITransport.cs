5411ce879838a4d52f09ef53d12934642de23fb1
D4Gj <inzadimonax@gmail.com>
2019-12-26 12:30:26 +0400

    Created Lab_5

--- WindowsFormsCars/WindowsFormsCars/ITransport.cs
+++ WindowsFormsCars/WindowsFormsCars/ITransport.cs
@@ -12,5 +12,6 @@ namespace WindowsFormsCars
         void SetPosition(int x, int y, int width, int height);
         void MoveTransport(Direction direction);
         void Draw(Graphics g);
+        void SetMainColor(Color color);
     }
 }
